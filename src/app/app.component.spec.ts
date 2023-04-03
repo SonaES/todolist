586fbd2238be8ba09239db323508f3e0d134815b
sona sudhakaran <sonasudhakaran12@gmail.com>
2023-04-03 11:59:13 +0530

    Todolist

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'todolist'`, () => {
+  it(`should have as title 'ToDoList'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('todolist');
+    expect(app.title).toEqual('ToDoList');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('todolist app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('ToDoList app is running!');
   });
 });
